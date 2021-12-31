c071bda0eb2b83938c3a44ccf99bc53d0e2f292f
shu Lin <bbqlinshu@gmail.com>
2021-12-30 23:01:43 -0500

    Completed assignment

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,13 @@
 function reverseString(str) {
   // type your code here
+  //if it's a empty string, return empty
+  if (str.length === 0){
+    return ""
+  } else {
+  /*otherwise reversString function recurses taking rest of the current string except the first letter as parameter, 
+  then add back the first letter of current string to the back of returned result. */
+    return reverseString(str.slice(1)) + str[0]    
+  }
 }
 
 if (require.main === module) {
@@ -11,6 +19,16 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("");
+
+  console.log("Expecting: '");
+  console.log("=>", reverseString(""));
+
+  console.log("");
+
+  console.log("Expecting: 'oof'");
+  console.log("=>", reverseString("foo"));
 }
 
 module.exports = reverseString;
